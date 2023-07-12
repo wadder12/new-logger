2ef25fa21b82db218126a54c1084ea3400668f23
wadder12 <awade75009@gmail.com>
2023-07-12 22:10:36 +0000

    Wednesday, July 12, 2023 at 5:10 PM Central Daylight Time

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import nextcord
 from nextcord.ext import commands
 from dotenv import load_dotenv
 import os
-import psutil
 from termcolor import cprint
 import time
 
@@ -18,46 +17,7 @@ bot = commands.Bot(command_prefix='q', intents=nextcord.Intents.all(), activity=
 
 @bot.event
 async def on_ready():
-
-  print('---------------------------------------')
-  
-  # Log details on bot user
-  bot_user = bot.user
-  print(f'Logged in as: {bot_user.name} (ID: {bot_user.id})')
-
-  # Log guild count  
-  guild_count = len(bot.guilds)
-  print(f'Connected to {guild_count} guilds')
-
-  # Log member count
-  member_count = 0
-  for guild in bot.guilds:
-    member_count += guild.member_count
-  print(f'Serving {member_count} members total')
-
-  # Log channels 
-  channel_count = 0
-  for guild in bot.guilds:
-    channel_count += len(guild.channels)
-  print(f'Access to {channel_count} channels')
-
-  # Log other detailed analytics
-  voice_clients = len(bot.voice_clients)
-  print(f'{voice_clients} active voice clients')
-  
-  uptime = (bot.uptime).strftime("%H:%M:%S")
-  print(f'Uptime: {uptime}')
-
-  cpu_usage = psutil.cpu_percent()
-  print(f'CPU Usage: {cpu_usage}%')
-
-  memory_usage = psutil.virtual_memory().percent
-  print(f'Memory Usage: {memory_usage}%')
-
-  disk_usage = psutil.disk_usage('/').percent
-  print(f'Disk Usage: {disk_usage}%')
-
-  print('---------------------------------------')
+    print('---------------------------------------')
     
 
 
