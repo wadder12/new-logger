c4682d33aeb5987ec0673f26eddd9d62db94147b
wadder12 <awade75009@gmail.com>
2023-07-13 01:03:24 +0000

    Wednesday, July 12, 2023 at 8:03 PM Central Daylight Time

--- commands/cogs/global/Inviter.py
+++ commands/cogs/global/Inviter.py
@@ -1,30 +1,29 @@
+import datetime
 import nextcord
 from nextcord.ext import commands
-from nextcord.utils import datetime
 
 class InviteLogger(commands.Cog):
 
-    def __init__(self, bot):
-        self.bot = bot
+  def __init__(self, bot):
+      self.bot = bot  
 
-    @commands.Cog.listener()
-    async def on_guild_join(self, guild):
-        
-        inviter = None
-        
-        if guild.me.guild_permissions.view_audit_log:
-            invite_log = await guild.audit_logs(limit=1, action=nextcord.AuditLogAction.bot_add).flatten()
-            invite = invite_log[0]
-            inviter = invite.user
-            
-        if inviter:
-            embed = nextcord.Embed(title='Thanks for the invite!', 
-                                   description=f'Thank you {inviter.mention} for inviting me to {guild.name}!', 
-                                   color=nextcord.Color.green())
-            embed.set_thumbnail(url=self.bot.user.avatar.url)
-            embed.timestamp = datetime.utcnow()
-            
-            await inviter.send(embed=embed)
-            
+  @commands.Cog.listener()
+  async def on_guild_join(self, guild):
+  
+    inviter = None
+    if guild.me.guild_permissions.view_audit_log:
+      invite_log = await guild.audit_logs(limit=1, action=nextcord.AuditLogAction.bot_add).flatten()
+      invite = invite_log[0]
+      inviter = invite.user
+
+    if inviter:
+      current_time = datetime.datetime.utcnow()
+      
+      embed = nextcord.Embed(title='Thanks for inviting me!', description=f'Thank you {inviter.mention} for inviting me to {guild.name}!')
+      embed.set_thumbnail(url=self.bot.user.avatar.url)
+      embed.timestamp = current_time
+      
+      await inviter.send(embed=embed)
+      
 def setup(bot):
-    bot.add_cog(InviteLogger(bot))
+  bot.add_cog(InviteLogger(bot))
